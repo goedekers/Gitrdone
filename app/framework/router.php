9991719d34e04b6590242665e4a9a22776fba85c
Craig Russell <craig.russell@goedekers.com>
2015-08-28 11:20:33 -0500

    Update router

--- app/framework/router.php
+++ app/framework/router.php
@@ -4,10 +4,14 @@ class Router {
 	public static function route() {
 		$baseUri = preg_replace('/\/$/', '', App::get('config')->baseUri) . '/';
 		$path = strlen($_SERVER['REQUEST_URI']) > strlen($baseUri) ? substr($_SERVER['REQUEST_URI'], strlen($baseUri)) : 'index/index';
-		$path = preg_replace('/[^a-z0-9\-_\/]/Si', '', str_replace(array("\0", '.'), '', urldecode($path)));		//sanitize the path
+		$path = preg_replace('/\?.*/S', '', $path);
+		$path = preg_replace('/[^a-z0-9\-_\/\.]/Si', '', str_replace("\0", '', urldecode($path)));		//sanitize the path
 		$path = explode('/', $path);
 		if(sizeof($path) == 1)
 			$path[] = 'index';
+		$path[0] = str_replace('.', '', $path[0]);
+		if(isset($path[1]))
+			$path[1] = str_replace('.', '', $path[1]);
 		App::set('route', $path);
 
 		$valid = true;
@@ -19,7 +23,7 @@ class Router {
 		}
 		else
 			$valid = false;
-
+		
 		if($valid) {
 			App::set('route', $path);
 
